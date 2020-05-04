219bb2481fea2a2fbed1fcd912e535fe811dbdc7
SasiDharKM <sasidhar.km97@gmail.com>
2020-05-04 10:32:18 -0400

    Replacement and testing done

--- CSC_541_Assn_3/assn_3.c
+++ CSC_541_Assn_3/assn_3.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/time.h>
 #include <limits.h>
+#include <stdbool.h>
 
 void print_standard(struct timeval  begin, struct timeval finish){
 
@@ -55,9 +56,11 @@ void merge(int start, char input_filename[], FILE *output_file, int append_no, c
         j++;
       }
 
-      if(tem != -1 ||(tem < min && tem != -1)){
-        min_index = i;
-        min = tem;
+      if(tem != -1 ){
+        if(tem < min){
+          min_index = i;
+          min = tem;
+        }
       }
       else if(run_files[i]){
 
@@ -75,7 +78,7 @@ void merge(int start, char input_filename[], FILE *output_file, int append_no, c
           }
           j++;
         }
-        if(tem != -1 || (tem != -1 && tem < min)){
+        if (tem != -1 && tem < min){
           min_index = i;
           min = tem;
         }
@@ -85,12 +88,6 @@ void merge(int start, char input_filename[], FILE *output_file, int append_no, c
         }
       }
       i++;
-    }
-    output[index++] = min;
-
-    if(index >= 1000){
-      index = 0;
-      fwrite(output, sizeof(int), 1000, output_file);
     }
 
     if(min_index != -1){
@@ -102,6 +99,13 @@ void merge(int start, char input_filename[], FILE *output_file, int append_no, c
         }
         i++;
       }
+      output[index] = min;
+      index = index + 1;
+
+      if(index >= 1000){
+        index = 0;
+        fwrite(output, sizeof(int), 1000, output_file);
+      }
     }
   }
 
@@ -111,7 +115,7 @@ void merge(int start, char input_filename[], FILE *output_file, int append_no, c
   }
 }
 
-void heapify(int heap_array[], int i, int n){
+void heapify(int *heap_array, int i, int n){
 
   int smallest = i;
   int left = 2*i + 1;
@@ -131,10 +135,10 @@ void heapify(int heap_array[], int i, int n){
   }
 }
 
-void heap_sort(int heap_array[], int n){
+void heap_sort(int *heap_array, int n){
   int i = n/2;
   while(i>=0){
-    heapify(heap, i, n);
+    heapify(heap_array, i, n);
     i--;
   }
 }
@@ -160,9 +164,9 @@ void sort_write_blocks(char input_filename[], FILE *input_file, int block_len, i
 int main(int argc, char *argv[]){
 
 int mergesort_method_flag = 10;
-char mergesort_method[] = argv[1];
-char index_filename[] = argv[2];
-char sorted_index_filename[] = argv[3];
+char *mergesort_method = argv[1];
+char *index_filename = argv[2];
+char *sorted_index_filename = argv[3];
 
 struct timeval begin;
 struct timeval finish;
@@ -177,26 +181,28 @@ else if (!strcmp(mergesort_method,"--replacement")){
   mergesort_method_flag = 3;
 }
 
-FILE *index_file = fopen(index_filename, "r");
-fseek(index_file, 0, SEEK_END);
-size_t len_size = (size_t)ftell(index_file)/sizeof(int);
-
-int no_of_runs = (int)len_size/1000;
-if(((int)len_size)%1000 != 0){
-  no_of_runs = no_of_runs + 1;
-}
-rewind(index_file);
-int run = 0;
-while(run < no_of_runs - 1){
-  sort_write_blocks(index_filename, index_file, 1000, run);
-  run++;
-}
-sort_write_blocks(index_filename, index_file, (int)len_size -(1000*no_of_runs - 1),no_of_runs - 1);
-fclose(index_file);
 
 switch(mergesort_method_flag){
   case 1: {
     gettimeofday(&begin, NULL);
+
+    FILE *index_file = fopen(index_filename, "r");
+    fseek(index_file, 0, SEEK_END);
+    size_t len_size = (size_t)ftell(index_file)/sizeof(int);
+
+    int no_of_runs = (int)len_size/1000;
+    if(((int)len_size)%1000 != 0){
+      no_of_runs = no_of_runs + 1;
+    }
+    rewind(index_file);
+    int run = 0;
+    while(run < no_of_runs - 1){
+      sort_write_blocks(index_filename, index_file, 1000, run);
+      run++;
+    }
+    sort_write_blocks(index_filename, index_file, (int)len_size -(1000*(no_of_runs - 1)),no_of_runs - 1);
+    fclose(index_file);
+
 
     FILE *sorted_index_file = fopen(sorted_index_filename, "w");
     merge(0, index_filename, sorted_index_file, 4,".%03d", no_of_runs);
@@ -208,6 +214,23 @@ switch(mergesort_method_flag){
   }
   case 2: {
     gettimeofday(&begin, NULL);
+    FILE *index_file = fopen(index_filename, "r");
+    fseek(index_file, 0, SEEK_END);
+    size_t len_size = (size_t)ftell(index_file)/sizeof(int);
+
+    int no_of_runs = (int)len_size/1000;
+    if(((int)len_size)%1000 != 0){
+      no_of_runs = no_of_runs + 1;
+    }
+    rewind(index_file);
+    int run = 0;
+    while(run < no_of_runs - 1){
+      sort_write_blocks(index_filename, index_file, 1000, run);
+      run++;
+    }
+    sort_write_blocks(index_filename, index_file, (int)len_size -(1000*(no_of_runs - 1)),no_of_runs - 1);
+    //fclose(index_file);
+
     char super_run_filename[strlen(index_filename) + 10];
     FILE *sorted_index_file = fopen(sorted_index_filename, "w");
 
@@ -237,6 +260,7 @@ switch(mergesort_method_flag){
 
     merge(0, index_filename, sorted_index_file, 10, ".super.%03d", no_of_super_runs);
     fclose(sorted_index_file);
+    fclose(index_file);
 
     gettimeofday(&finish, NULL);
     print_standard(begin, finish);
@@ -244,7 +268,161 @@ switch(mergesort_method_flag){
   }
   case 3: {
     gettimeofday(&begin, NULL);
-    //call replacement method
+    FILE *index_file = fopen(index_filename, "r");
+    fseek(index_file, 0, SEEK_END);
+    size_t len_size = (size_t)ftell(index_file)/sizeof(int);
+    rewind(index_file);
+
+    FILE *sorted_index_file = fopen(sorted_index_filename, "w");
+
+    char run_filename[strlen(index_filename) + 4];
+    int output[1000], heap_array[750], input_buffer[250];
+    int run_append_no = 0;
+
+    if((int)len_size > 1000){
+
+      fread(heap_array, sizeof(int), 750, index_file);
+      fread(input_buffer, sizeof(int), 250, index_file);
+
+      heap_sort(heap_array, 750);
+
+      char temp[4];
+      sprintf(temp, ".%03d",run_append_no);
+      strcpy(run_filename, index_filename);
+      strcat(run_filename, temp);
+      FILE *run_file = fopen(run_filename, "w");
+
+
+      int output_index = 0, buffer_index = 0, last = 0, last_buffer = 0;
+      int left = (int)len_size - 1000, heap_size = 750;
+
+      while(true){
+
+        if((last != 0 && (buffer_index - last != 0)) || (last == 0)){
+          if(heap_size <= 0){
+            if(output_index > 0){
+              fwrite(output, sizeof(int), output_index, run_file);
+              output_index = 0;
+            }
+            heap_size = 750;
+            heap_sort(heap_array, heap_size);
+
+            run_append_no = run_append_no + 1;
+            sprintf(temp, ".%03d",run_append_no);
+            strcpy(run_filename, index_filename);
+            strcat(run_filename, temp);
+            fclose(run_file);
+            run_file = fopen(run_filename, "w");
+
+          }
+          else if(heap_size > 0){
+            output[output_index] = heap_array[0];
+            output_index = output_index + 1;
+            if(heap_array[0] <= input_buffer[buffer_index]){
+              heap_array[0] = input_buffer[buffer_index];
+              buffer_index = buffer_index + 1;
+              heap_sort(heap_array, heap_size);
+
+              if(buffer_index == 250){
+                int buffer_read_len = 0;
+                if(left < 250){
+                  buffer_read_len = left;
+                }
+                else{
+                  buffer_read_len = 250;
+                }
+                if(buffer_read_len != 0){
+                  buffer_index = 0;
+                  fread(input_buffer, sizeof(int), buffer_read_len, index_file);
+                }
+                left = left - buffer_read_len;
+                if(left == 0 && last == 0){
+                  last = buffer_read_len;
+                }
+              }
+            }
+            else if(heap_array[0] > input_buffer[buffer_index]){
+              heap_array[0] = heap_array[heap_size - 1];
+              heap_array[heap_size - 1] = input_buffer[buffer_index];
+              buffer_index = buffer_index + 1;
+
+              heap_size = heap_size - 1;
+              heap_sort(heap_array, heap_size);
+
+              if(buffer_index == 250){
+                int buffer_read_len = 0;
+                if(left < 250){
+                  buffer_read_len = left;
+                }
+                else{
+                  buffer_read_len = 250;
+                }
+                if(buffer_read_len != 0){
+                  buffer_index = 0;
+                  fread(input_buffer, sizeof(int), buffer_read_len, index_file);
+                }
+
+                left = left - buffer_read_len;
+                if(left == 0 && last == 0){
+                  last = buffer_read_len;
+                }
+              }
+            }
+            if(output_index == 1000){
+              output_index = 0;
+              fwrite(output, sizeof(int), 1000, run_file);
+            }
+          }
+        }
+        else{
+          if(heap_size <= 0){
+            if(output_index > 0){
+              fwrite(output, sizeof(int), output_index, run_file);
+              output_index = 0;
+            }
+            run_append_no = run_append_no + 1;
+            sprintf(temp, ".%03d",run_append_no);
+            strcpy(run_filename, index_filename);
+            strcat(run_filename, temp);
+            fclose(run_file);
+            run_file = fopen(run_filename, "w");
+            size_t sort_len = (size_t)(750-last_buffer);
+            qsort(&heap_array[last_buffer], sort_len, sizeof(int), comparision);
+            fwrite(&heap_array[last_buffer], sizeof(int), (int)sort_len, run_file);
+            fclose(run_file);
+            break;
+          }
+          else if(heap_size > 0){
+            output[output_index] = heap_array[0];
+            output_index = output_index + 1;
+
+            heap_size = heap_size - 1;
+            heap_array[0] = heap_array[heap_size];
+            heap_sort(heap_array, heap_size);
+
+            if(output_index == 1000){
+              output_index = 0;
+              fwrite(output, sizeof(int), 1000, run_file);
+            }
+            last_buffer = last_buffer + 1;
+          }
+        }
+      }
+      if(output_index > 0){
+        fwrite(output, sizeof(int), output_index, run_file);
+        fclose(run_file);
+      }
+      run_append_no = run_append_no + 1;
+      merge(0, index_filename, sorted_index_file, 4, ".%03d", run_append_no);
+    }
+    else if((int)len_size <= 1000) {
+      int inputs[(int)len_size];
+      fread(inputs, sizeof(int), (int)len_size, index_file);
+      qsort(inputs, (int)len_size, sizeof(int), comparision);
+      fwrite(inputs, sizeof(int), (int)len_size, sorted_index_file);
+    }
+    fclose(index_file);
+    fclose(sorted_index_file);
     gettimeofday(&finish, NULL);
     print_standard(begin, finish);
     break;
